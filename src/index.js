6508dcada90ebd2c14d9e3297728d95af141f3ee
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-06-17 15:16:40 +0300

    Add configurations for Phaser in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('heey')
+import MainScene from "./MainScene.js";
+
+const config = {
+    width: 600,
+    height: 512,
+    backgroundColor: '#999999',
+    type: Phaser.AUTO,
+    parent: 'survival-game',
+    scene: [MainScene],
+    scale: {
+        zoom: 2,
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            debug: true,
+            gravity: { y: 0 },
+        }
+    },
+    plugins: {
+        scene: [
+            {
+                plugin: PhaserMatterCollisionPlugin,
+                key: 'matterCollision',
+                mapping: 'matterCollision'
+            }
+        ]
+    }
+}
+
+
+new Phaser.Game(config);
